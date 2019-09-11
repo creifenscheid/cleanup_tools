10ac16739d28e6b9ed2a9d46846537778b4eb926
Christian Reifenscheid <christian.reifenscheid.2112@gmail.com>
2019-09-11 18:37:21 +0200

    [Toolbar] - Icon
    Add and set toolbar icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,13 @@ defined ('TYPO3_MODE') or die();
 
 // Register toolbar item
 $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1435433112] = \SPL\SplCleanupTools\Backend\Toolbar\CleanUpToolbarItem::class;
+
+// Register icons
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Imaging\IconRegistry::class
+);
+$iconRegistry->registerIcon(
+    'tx-splcleanuptools-toolbar',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:spl_cleanup_tools/Resources/Public/Icons/tx_splcleanuptools_toolbar.svg']
+);
