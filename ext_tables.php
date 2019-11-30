3cc48110b44a279396b31e6add1669fa5ba75efe
Christian Reifenscheid <christian.reifenscheid.2112@gmail.com>
2019-11-30 23:38:55 +0100

    Cleanup | ext_tables
    Cleanup module registration

--- ext_tables.php
+++ ext_tables.php
@@ -4,18 +4,18 @@ defined('TYPO3_MODE') or die();
 
 if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        $extensionName = 'SPL.SplCleanupTools',
-        $mainModuleName = 'tools',
-        $subModuleName = 'Cleanup',
-        $position = '',
-        $controllerActions = [
+        'SPL.SplCleanupTools', // extensionName
+        'tools', // mainModuleName
+        'Cleanup', // subModuleName
+        '', // position
+        [
             'Cleanup' => 'index,cleanup',
             'Toolbar' => 'main'
-        ],
-        $moduleConfiguration = [
+        ], // controllerActions
+        [
             'access' => 'admin',
             'icon' => 'EXT:spl_cleanup_tools/ext_icon.svg',
             'labels' => 'LLL:EXT:spl_cleanup_tools/Resources/Private/Language/locallang_mod.xlf',
-        ]
+        ] // moduleConfiguration
     );
 }
