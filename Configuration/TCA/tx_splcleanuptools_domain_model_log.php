2c707acfb7bd15f9b4d0b1c04549de4f10f986de
Christian Reifenscheid <christian.reifenscheid.2112@gmail.com>
2020-02-16 20:26:27 +0100

    Improve | Log tca
    Adjust table labeling

--- Configuration/TCA/tx_splcleanuptools_domain_model_log.php
+++ Configuration/TCA/tx_splcleanuptools_domain_model_log.php
@@ -4,7 +4,7 @@ return [
     'ctrl' => [
         'title' => 'LLL:EXT:spl_cleanup_tools/Resources/Private/Language/locallang_tca.xlf:tx_splcleanuptools_domain_model_log',
         'label' => 'utility',
-        'label_alt' => 'action, crdate',
+        'label_alt' => 'action, crdate, processing_context',
         'label_alt_force' => 1,
         'iconfile' => 'EXT:spl_cleanup_tools/Resources/Public/Icons/tx_splcleanuptools_domain_model_log.svg',
         'sortby' => 'crdate',
